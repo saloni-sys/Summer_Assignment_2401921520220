0b2e3f11311099ec80aaec87871d2784ea167c2f
Saloni singh <salini1abc@gmail.com>
2026-06-04 13:05:29 +0530

    Implement sortedSquares function to square sorted array

---
+++ Week_1/Day_3/Square the sorted array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+
+        int n=nums.size();
+
+        vector<int>result(n);
+        int i=0,j=n-1;
+        int k=n-1;//to store result from right end
+
+        while(k>=0){
+            int a=nums[i]*nums[i];
+            int b=nums[j]*nums[j];
+
+            if(a>b){
+                result[k]=a;
+                i++;
+            }else{
+
+                result[k]=b;
+                j--;
+            }
+            k--;
+
+        }
+       return result; 
+    }
+};
