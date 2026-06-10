dc7c5f47e75fcb17b5066904baf956eee0f943bc
Saloni singh <salini1abc@gmail.com>
2026-06-10 12:55:07 +0530

    Add solution for finding first occurrence of substring

---
+++ Week_2/Day_3/Index of first occurence.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+
+        int m = haystack.size();
+        int n = needle.size();
+
+        if (n > m) return -1;
+
+        for (int i = 0; i <= m - n; i++) {
+
+            int j;
+
+            for (j = 0; j < n; j++) {
+
+                if (haystack[i + j] != needle[j]) {
+                    break;
+                }
+            }
+
+            if (j == n) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+};
