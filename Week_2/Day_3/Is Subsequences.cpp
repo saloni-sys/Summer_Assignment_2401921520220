59a3452fc7fa490156803d5c6c00d5cd418b5d9b
Saloni singh <salini1abc@gmail.com>
2026-06-10 19:19:17 +0530

    Implement isSubsequence function in C++

---
+++ Week_2/Day_3/Is Subsequences.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m = s.length();
+        int n = t.length();
+
+        map<char, vector<int>> mp;
+
+        for (int i = 0; i < n; i++) {
+            mp[t[i]].push_back(i);
+        }
+
+        int prev = -1;
+
+        for (int i = 0; i < m; i++) {
+            char ch = s[i];
+
+            if (mp.find(ch) == mp.end()) {
+                return false;
+            }
+
+            vector<int>& indices = mp[ch];
+
+            auto it = upper_bound(indices.begin(), indices.end(), prev);
+
+            if (it == indices.end()) {
+                return false;
+            }
+
+            prev = *it;
+        }
+
+        return true;
+    }
+};
