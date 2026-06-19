9dc428d339d97679f6d9fa687e47fe37a023ec42
Saloni singh <salini1abc@gmail.com>
2026-06-19 20:26:54 +0530

    Implement RecentCounter class for tracking pings

---
+++ Week_3/Day_5/Number of Recent Calls.cpp
@@ -0,0 +1,19 @@
+class RecentCounter {
+public:
+queue<int>q;
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+     q.push(t);
+     int low=t-3000;
+     while(!q.empty()&&q.front()<low){
+        q.pop();
+     }
+
+     return q.size();
+        
+    }
+};
+
