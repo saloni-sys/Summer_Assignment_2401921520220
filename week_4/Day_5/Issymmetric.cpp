09dbf2a53e42a624e15f820a1734d5853d3f834b
Saloni singh <salini1abc@gmail.com>
2026-06-27 13:09:07 +0530

    Convert isSymmetric from Python to C++

---
+++ week_4/Day_5/Issymmetric.cpp
@@ -0,0 +1,27 @@
+
+
+class Solution {
+public:
+    bool solve(TreeNode* left, TreeNode* right) {
+
+        if (left == NULL && right == NULL)
+            return true;
+
+        if (left == NULL || right == NULL)
+            return false;
+
+        if (left->val != right->val)
+            return false;
+
+        return solve(left->left, right->right) &&
+               solve(left->right, right->left);
+    }
+
+    bool isSymmetric(TreeNode* root) {
+
+        if (root == NULL)
+            return true;
+
+        return solve(root->left, root->right);
+    }
+};
